4349613d2a4ca0fc097a4493ebe43dd4d9254abe
Prog'z <39605375+ThomasProg@users.noreply.github.com>
2022-04-14 15:10:46 +0200

    debug utils messenger added

--- include/Context.hpp
+++ include/Context.hpp
@@ -6,28 +6,66 @@
 namespace ARE
 {
 
+struct VulkanContextDebugCreationInfo
+{
+    VkDebugUtilsMessageSeverityFlagsEXT messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT
+                                                        // | VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT
+                                                        | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
+                                                        | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+
+    VkDebugUtilsMessageTypeFlagsEXT messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT 
+                                                | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT 
+                                                | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+};
+
 struct VulkanContextCreateInfo
 {
     std::vector<const char*> requiredExtensions;
 
     const char* appName;
     uint32_t appVersion;
+
+    VulkanContextDebugCreationInfo* debugInfo = nullptr;
 };
 
 class Context
 {
 private:
     VkInstance instance;
+    VkDebugUtilsMessengerEXT debugMessenger;
+
+    bool isDebugEnabled;
 
     const char* engineName = "Aphrodite's Rendering Engine";
     uint32_t engineVersion = VK_MAKE_VERSION(1, 0, 0);
     uint32_t apiVersion = VK_API_VERSION_1_0;
 
     VkApplicationInfo getAppInfo(const VulkanContextCreateInfo& contextCreateInfo);
+    VkDebugUtilsMessengerCreateInfoEXT getDebugUtilsMessengerCreateInfo(const VulkanContextCreateInfo& contextCreateInfo);
+
+    VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, 
+                                        const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, 
+                                        const VkAllocationCallbacks* pAllocator, 
+                                        VkDebugUtilsMessengerEXT* pDebugMessenger);
+
+    void DestroyDebugUtilsMessengerEXT(VkInstance instance, 
+                                        VkDebugUtilsMessengerEXT debugMessenger, 
+                                        const VkAllocationCallbacks* pAllocator); 
+
+    void createDebugContext(const VulkanContextCreateInfo& contextCreateInfo);
+    void createReleaseContext(const VulkanContextCreateInfo& contextCreateInfo);
+
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, 
+                                                        VkDebugUtilsMessageTypeFlagsEXT messageType, 
+                                                        const VkDebugUtilsMessengerCallbackDataEXT* callbackData, 
+                                                        void* userData);
 
 public:
     Context(const VulkanContextCreateInfo& contextCreateInfo);
     ~Context();
+
+    void printDebugMessage(const char* message);
 };
 
 
--- src/Context.cpp
+++ src/Context.cpp
@@ -7,6 +7,82 @@ namespace ARE
 
 Context::Context(const VulkanContextCreateInfo& contextCreateInfo)
 {
+    isDebugEnabled = contextCreateInfo.debugInfo != nullptr;
+
+    if (isDebugEnabled)
+        createDebugContext(contextCreateInfo);
+    else
+        createReleaseContext(contextCreateInfo);
+}
+
+void Context::createDebugContext(const VulkanContextCreateInfo& contextCreateInfo)
+{
+    uint32_t nbLayers;
+    vkEnumerateInstanceLayerProperties(&nbLayers, nullptr);
+    std::vector<VkLayerProperties> availableLayers(nbLayers);
+    vkEnumerateInstanceLayerProperties(&nbLayers, availableLayers.data());
+
+    auto predicate = [](const VkLayerProperties& availableLayer) { return (strcmp(availableLayer.layerName, "VK_LAYER_KHRONOS_validation") == 0); };
+    if (std::find_if(availableLayers.begin(), availableLayers.end(), predicate) == availableLayers.end())
+    {
+        throw std::runtime_error("Validations layers are not available for debugging.");
+    }
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+    VkApplicationInfo appInfo = getAppInfo(contextCreateInfo);
+    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo = getDebugUtilsMessengerCreateInfo(contextCreateInfo);
+
+    std::vector<const char*> requiredExtensions = contextCreateInfo.requiredExtensions;
+    requiredExtensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+
+    createInfo.pApplicationInfo = &appInfo;
+    createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
+    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+    createInfo.pNext = &debugCreateInfo;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+    {
+        throw std::runtime_error("Instance could not be created.");
+    }
+
+    if (CreateDebugUtilsMessengerEXT(instance, &debugCreateInfo, nullptr, &debugMessenger) != VK_SUCCESS) 
+    {
+        vkDestroyInstance(instance, nullptr);
+
+        throw std::runtime_error("Failed to create debug messenger!");
+    }
+}
+
+VkDebugUtilsMessengerCreateInfoEXT Context::getDebugUtilsMessengerCreateInfo(const VulkanContextCreateInfo& contextCreateInfo)
+{
+    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo = {};
+    debugCreateInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    debugCreateInfo.messageSeverity = contextCreateInfo.debugInfo->messageSeverity;
+    debugCreateInfo.messageType = contextCreateInfo.debugInfo->messageType;
+    debugCreateInfo.pfnUserCallback = debugCallback;
+    debugCreateInfo.pUserData = this;
+    return debugCreateInfo;
+}
+
+
+VKAPI_ATTR VkBool32 VKAPI_CALL Context::debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* callbackData, void* userData) 
+{
+    Context* context = static_cast<Context*>(userData);
+
+    context->printDebugMessage(callbackData->pMessage);
+
+    return VK_FALSE;
+}
+
+void Context::printDebugMessage(const char* message)
+{
+    std::cout << "--- Vulkan : " << message << std::endl;
+}
+
+void Context::createReleaseContext(const VulkanContextCreateInfo& contextCreateInfo)
+{
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 
@@ -22,6 +98,8 @@ Context::Context(const VulkanContextCreateInfo& contextCreateInfo)
     }
 }
 
+
+
 VkApplicationInfo Context::getAppInfo(const VulkanContextCreateInfo& contextCreateInfo)
 {
     VkApplicationInfo appInfo;
@@ -36,9 +114,33 @@ VkApplicationInfo Context::getAppInfo(const VulkanContextCreateInfo& contextCrea
 
 Context::~Context()
 {
+    if (isDebugEnabled) 
+	    DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+
     vkDestroyInstance(instance, nullptr);
 }
 
 
+VkResult Context::CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) 
+{
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != nullptr) 
+    {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    } 
+    else 
+    {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void Context::DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) 
+{
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (func != nullptr) 
+    {
+        func(instance, debugMessenger, pAllocator);
+    }
+}
 
 }
